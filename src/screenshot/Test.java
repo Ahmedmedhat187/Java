91299af313d051b819ca1af79bbe1fc9689e3772
ahmedmedhat187 <ahmed.medhat187@gmail.com>
2017-02-03 22:28:16 +0200

    "first commit"

---
+++ src/screenshot/Test.java
@@ -0,0 +1,12 @@
+
+package screenshot;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        Screenshots s = new Screenshots();
+        s.setVisible(true);
+    }
+    
+    
+}
